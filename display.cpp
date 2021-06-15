ad9014196da8167af6a319963709d6bf43a44016
jj1guj <57407826+jj1guj@users.noreply.github.com>
2021-06-15 15:42:58 +0900

    名前がかぶるので改名

--- display.cpp
+++ display.cpp
@@ -1,8 +1,10 @@
 #include "gui.hpp"
 //GUIとして表示用の関数たち
 
-void disp(void){
+void display(void){
+    Board board;
     glClear(GL_COLOR_BUFFER_BIT|GL_COLOR_BUFFER_BIT);
     draw_board();
+    draw_phase(board);
     glutSwapBuffers();
 }
--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main(int argc,char** argv){
     glutCreateWindow("dekunobou-gui");
     glClearColor(0,0.31,0.18,1);
     gluOrtho2D(0,600,600,0);
-    glutDisplayFunc(disp);
+    glutDisplayFunc(display);
     glutMainLoop();
 }
